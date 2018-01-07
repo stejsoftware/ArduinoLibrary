2a2f031997c842934eb3f39f66b8090c7b330a7a
Jonathan Meyer <jon@stejsoftware.com>
2018-01-07 08:10:43 -0500

    ...

--- examples/Timer/Timer.ino
+++ examples/Timer/Timer.ino
@@ -1,1 +1,34 @@
-#include "Timer.h"
+#include <Timer.h>
+
+#define SECOND 1000
+#define MINUTE 60 * SECOND
+#define HOUR 60 * SECOND
+
+void second()
+{
+    Serial.println("Tick");
+}
+
+void minute()
+{
+    Serial.println("It has been a minute.");
+}
+
+void wake()
+{
+    Serial.println("Time to wake!")
+}
+
+void setup()
+{
+    Serial.begin(9600);
+
+    Timer.alarm(wake, HOUR);
+    Timer.repeat(second, SECOND);
+    Timer.repeat(minute, MINUTE);
+}
+
+void loop()
+{
+    Timer.run();
+}
